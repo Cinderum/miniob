4a0105f78f1a2b86838e13ed549cd42afc5cb79f
陈皮 <3364107383@qq.com>
2025-03-17 06:18:21 +0000

    update

--- src/observer/storage/table/table.cpp
+++ src/observer/storage/table/table.cpp
@@ -127,6 +127,39 @@ RC Table::create(Db *db, int32_t table_id, const char *path, const char *name, c
   return rc;
 }
 
+RC Table::drop(const char *path)
+{
+  if (::remove(path) < 0) {
+    LOG_ERROR("fialed to delete table file. filename=%s, errmsg=%s", path, strerror(errno));
+    return RC::INTERNAL;
+  }
+
+  RC rc = RC::SUCCESS;
+
+  string             data_file = table_data_file(base_dir_.c_str(), table_meta_.name());
+  BufferPoolManager &bpm       = db_->buffer_pool_manager();
+  rc                           = bpm.remove_file(data_file.c_str());
+  data_buffer_pool_ = nullptr;
+
+  if (rc != RC::SUCCESS) {
+    LOG_ERROR("Failed to remove disk buffer pool of data file. file name=%s", data_file.c_str());
+    return rc;
+  }
+
+  if (record_handler_ != nullptr) {
+    delete record_handler_;
+    record_handler_ = nullptr;
+  }
+
+  for (auto &index : indexes_) {
+    index -> destroy();
+    delete index;
+    index = nullptr;
+  }
+
+  return RC::SUCCESS;
+}
+
 RC Table::open(Db *db, const char *meta_file, const char *base_dir)
 {
   // 加载元数据文件
--- src/observer/storage/table/table.h
+++ src/observer/storage/table/table.h
@@ -55,6 +55,14 @@ public:
       span<const AttrInfoSqlNode> attributes, StorageFormat storage_format);
 
   /**
+   * @brief drop一个表
+   * 
+   * @param path 
+   * @return RC 
+   */
+  RC drop(const char *path);
+
+  /**
    * 打开一个表
    * @param meta_file 保存表元数据的文件完整路径
    * @param base_dir 表所在的文件夹，表记录数据文件、索引数据文件存放位置
